20f4434555c6f17c1d3d953d8da6402864aac0a0
V-Dat <vandat.k21@gmail.com>
2022-03-31 16:17:52 +0700

    Modify page Day3 to Component & use SCSS

--- src/pages/Day3/Day3.js
+++ src/pages/Day3/Day3.js
@@ -1,66 +1,21 @@
-import "./index.css";
-import {useEffect} from "react"
-
+import "./index.scss";
+import Dialog from "../../component/Day3/Dialog.js";
+import SearchBox from "../../component/Day3/SearchBox.js";
+import Row from "../../component/ReUse/Row/Row";
+import Container from "../../component/ReUse/Container/Container";
+import Content from "../../component/ReUse/Content/Content";
 function Day3() {
-  console.log('Day3: render....')
-
-  const handleOpenModel = () =>{
-    const openModel = document.querySelector('.dialog--hide')
-    openModel.classList.toggle('dialog--show')
-  }
-
-  const handleCloseModel = () =>{
-    const openModel = document.querySelector('.dialog--hide')
-    openModel.classList.toggle('dialog--show')
-  }
-
-  useEffect(() => {
-    const searchBox = document.querySelector('.search-box')
-    const searchBox__input = document.querySelector('.search-box__input')
-    const searchBox__button = document.querySelector('.search-box__button')
-    
-      searchBox.addEventListener('click', function(e){
-        if(e.target.classList.contains('search-box__input')){
-          console.log('please input sthg')
-        }else{
-          searchBox__button.classList.toggle('search-box__button--active')
-          searchBox__input.focus();
-          searchBox__input.classList.toggle('search-box__input--active')
-      }})
-  } , [])
-
-
-
-
   return (
-    <>
-      <div className="day3">
-      <div className="dialog--hide ">
-        <div className="dialog__inner">
-          <div className="dialog__header">
-            <p>Welcome to my website</p>
-            <i className="fas fa-times" onClick={handleCloseModel} ></i>
-          </div>
-          <div className="dialog__body">
-            <h2>Modal</h2>
-            <p>Đây là phần nội dung thông báo</p>
-          </div>
-          <div className="dialog__footer">
-            <button onClick={handleCloseModel}>Close</button>
-          </div>
-        </div>
-      </div>
-      <button className="dialog__btnOpen" onClick={handleOpenModel}>Open Modal</button>
-
-    <div className='break' >  </div>
-
-      <div className="search-box">
-        <input className ="search-box__input"type="text" />
-        <button className ="search-box__button"><i className=' fa fa-search'></i></button>
-      </div>
-
-      </div>
-    </>
+    <Content className="day3 background-color">
+      <Container>
+        <Row>
+          <Dialog />
+        </Row>
+        <Row className="break">
+          <SearchBox />
+        </Row>
+      </Container>
+    </Content>
   );
 }
 
