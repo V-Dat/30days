e2f5dc1ee4d7b9edab0249b6e1d7cd00721fc060
V-Dat <vandat.k21@gmail.com>
2022-04-30 01:14:48 +0700

    Remove Unused Component

--- src/App.js
+++ src/App.js
@@ -25,35 +25,39 @@ import {
   Routes,
 } from "react-router-dom";
 import AppLayout from "./component/AppLayout/AppLayout";
+import ScrollToTop from "./component/ScrollToTop/ScrollToTop";
 
 function App() {
+
+
   return (
     <>
       <Suspense fallback={<div>...Loading</div>}>
         <Router>
-          <Routes>
-            <Route path="Login" element={<Login />} />
-            <Route path="Day1" element={<Day1 />} />
-            <Route path="/" element={<AppLayout />}>
-              <Route path="Day2" element={<Day2 />} />
-              <Route path="Day3" element={<Day3 />} />
-              <Route path="Day4" element={<Day4 />} />
-              <Route path="Day5" element={<Day5 />} />
-              <Route path="Day6" element={<Day6 />} />
-              <Route path="Day7" element={<Day7 />} />
-              <Route path="Day8" element={<Day8 />} />
-              <Route path="Day9" element={<Day9 />} />
-              <Route path="News" element={<News />} />
-              <Route path="Profile" element={<Profile />} />
-              <Route path="404NotFound" element={<NotFound />} />
-              <Route path="Forgot-password" element={<ForgotPassword />} />
-              <Route path="Register" element={<Register />} />
-              <Route path="/" element={<Homepage />} />
-              <Route path="*" element={<Navigate replace to="/404NotFound" />}/>
-            </Route>
-            <Route path="*" element={<Navigate replace to="/404NotFound" />} />
-          </Routes>
-          <Footer />
+          <ScrollToTop />
+            <Routes>
+              <Route path="Login" element={<Login />} />
+              <Route path="Day1" element={<Day1 />} />
+              <Route path="/" element={<AppLayout />}>
+                <Route path="Day2" element={<Day2 />} />
+                <Route path="Day3" element={<Day3 />} />
+                <Route path="Day4" element={<Day4 />} />
+                <Route path="Day5" element={<Day5 />} />
+                <Route path="Day6" element={<Day6 />} />
+                <Route path="Day7" element={<Day7 />} />
+                <Route path="Day8" element={<Day8 />} />
+                <Route path="Day9" element={<Day9 />} />
+                <Route path="News" element={<News />} />
+                <Route path="Profile" element={<Profile />} />
+                <Route path="404NotFound" element={<NotFound />} />
+                <Route path="Forgot-password" element={<ForgotPassword />} />
+                <Route path="Register" element={<Register />} />
+                <Route path="/" element={<Homepage />} />
+                <Route path="*" element={<Navigate replace to="/404NotFound" />} />
+              </Route>
+              <Route path="*" element={<Navigate replace to="/404NotFound" />} />
+            </Routes>
+            <Footer />
         </Router>
       </Suspense>
     </>
